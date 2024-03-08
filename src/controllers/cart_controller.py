593ccba1250615ff1a08b59c2e1f22438471cfdf
re-gis <irumvaregisdmc@gmail.com>
2024-03-08 16:10:32 +0200

    adding 'remove from cart'

--- src/controllers/cart_controller.py
+++ src/controllers/cart_controller.py
@@ -75,6 +75,7 @@ def addToCart(user):
             # Update the total quantity and total price of the cart
             cart.total_quantity += data["quantity"]
             cart.total_price += data["quantity"] * product.price
+            product.stock -= data["quantity"]
 
             db.session.commit()
 
@@ -134,3 +135,126 @@ def addToCart(user):
             status=500,
             mimetype="application/json",
         )
+
+
+@carts.route("/remove/<int:cartId>", methods=["PUT"])
+@token_required
+def removeFromCarts(user, cartId):
+    try:
+        cart = Carts.query.get(cartId)
+        if not cart:
+            return Response(
+                response=json.dumps(
+                    {
+                        "status": "not found",
+                        "message": "Cart not found!",
+                    }
+                ),
+                status=404,
+                mimetype="application/json",
+            )
+
+        # check if the cart is of the logged in user
+        if str(user["user_id"]) != str(cart.owner_id):
+            return Response(
+                response=json.dumps(
+                    {
+                        "status": "Forbidden",
+                        "message": "You are authorised to modify your own cart only!",
+                    }
+                ),
+                status=403,
+                mimetype="application/json",
+            )
+
+        data = request.json
+
+        if not "productId" in data:
+            return Response(
+                response=json.dumps(
+                    {
+                        "status": "Bad request",
+                        "message": "Product id is required!",
+                    }
+                ),
+                status=400,
+                mimetype="application/json",
+            )
+
+        # get the product
+        product = Product.query.get(data["productId"])
+
+        if not product:
+            return Response(
+                response=json.dumps(
+                    {
+                        "status": "not found",
+                        "message": "Product not found!",
+                    }
+                ),
+                status=404,
+                mimetype="application/json",
+            )
+
+        # get the cart and cartitem and then decrement the quantity of the product by one and update the price
+        for item in cart.cart_items:
+            if str(product.name) == str(item.product_name):
+                item.quantity -= 1
+                item.total_price = product.price * item.quantity
+                cart.total_price -= product.price
+                cart.total_quantity -= 1
+                # increment the product stock
+                product.stock += 1
+
+            if item.quantity == 0:
+                # remove it
+                cart.cart_items.remove(item)
+                product.stock += 1
+                db.session.commit()
+
+        db.session.commit()
+        return Response(
+            response=json.dumps(
+                {
+                    "status": "success",
+                    "message": "Item removed from cart successfully...",
+                    "cart": {
+                        "cart_id": cart.id,
+                        "owner": {
+                            "user_id": cart.owner.id,
+                            "name": cart.owner.username,
+                            "email": cart.owner.email,
+                        },
+                        "cart_items": [
+                            {
+                                "product_name": item.product_name,
+                                "quantity": item.quantity,
+                                "price": product.price,
+                                "total_price": item.total_price,
+                            }
+                            for item in cart.cart_items
+                        ],
+                        "total_quantity": cart.total_quantity,
+                        "total_price": cart.total_price,
+                    },
+                }
+            ),
+            status=200,
+            mimetype="application/json",
+        )
+
+    except Exception as e:
+        return Response(
+            response=json.dumps(
+                {
+                    "status": "failed",
+                    "message": "Internal server error...",
+                    "error": str(e),
+                }
+            ),
+            status=500,
+            mimetype="application/json",
+        )
+
+
+
